bfe2f9678ca9f230751c26e49872ee88db7c75c6
G-linch <514292298@qq.com>
2023-05-04 14:31:23 +0800

    替换字段值中的特定字符
    替换数字

---
+++ 替换字段值字符.py
@@ -0,0 +1,11 @@
+def add_36(text):
+    if "-36" not in text:
+        text = text.replace("-", "-36")
+    return text
+
+'''
+1.字段计算器中选择python
+2.点击显示代码块,将以上代码copy到预编辑脚本代码框中
+3.将add_36(text)复制到最下边代码框,将add_36(text)中text换成待修改的字段
+注意:python引用字段名称加双叹号,例:add_36(!TFH!)
+'''
