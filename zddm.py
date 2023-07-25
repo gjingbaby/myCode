2b9655e2952864853050a938dd7e554f6a8e44f3
G-linch <514292298@qq.com>
2023-07-25 08:38:45 +0800

    长注释改为双引号
    长注释改为双引号，arcgis pro工具会报错

--- zddm.py
+++ zddm.py
@@ -1,77 +1,48 @@
-# -*- coding: utf-8 -*-
-'''
-r"C:\Users\18292\Desktop\zrz\610632ZRZ.shp"
+﻿# -*- coding: utf-8 -*-
+"""
+python3.9
 
-功能描述：
-1.对一个要素类的属性表进行编号
-2.给每个ZDDM值分配一个ZDDMXH值,表示它在同一个ZDDM分组中的序号
-3.将编号结果写入属性表中的ZDDMXH字段
-参数说明：
-1.要处理的要素类的路径,例如r"C:\Users\18292\Desktop\zrz\610632ZRZ.shp"
-2.要素类中的ZDDM字段,表示宗地代码
-3.要素类中的ZDDMXH字段,表示宗地代码序号
-'''
-
-
-# import arcpy
-# # 假设你的要素类是feature_class，ZDDM和ZDDMXH是两个字段
-# # 创建一个字典，用来存储ZDDM和ZDDMXH的对应关系
-# zddm_dict = {}
-# # 创建一个游标，用来遍历要素类中的每一行
-# with arcpy.da.UpdateCursor(r"C:\Users\18292\Desktop\zrz\610632ZRZ.shp", ["ZDDM", "ZDDMXH"]) as cursor:
-#     # 对于每一行
-#     for row in cursor:
-#         # 获取当前行的ZDDM值
-#         zddm = row[0]
-#         # 如果这个值已经在字典中，说明已经有一个序号了
-#         if zddm in zddm_dict:
-#             # 就把这个序号加一，作为新的序号
-#             zddmxh = zddm_dict[zddm] + 1
-#             # 把这个序号赋值给当前行的ZDDMXH字段
-#             row[1] = zddmxh
-#             # 并且更新字典中的这个ZDDM值对应的序号
-#             zddm_dict[zddm] = zddmxh
-#         # 否则，说明这是一个新的ZDDM值，还没有序号
-#         else:
-#             # 就把序号设为1
-#             zddmxh = 1
-#             # 把这个序号赋值给当前行的ZDDMXH字段
-#             row[1] = zddmxh
-#             # 并且把这个ZDDM和ZDDMXH的对应关系存入字典中
-#             zddm_dict[zddm] = zddmxh
-#         # 更新当前行的值
-#         cursor.updateRow(row)
-
+解决相同字段内容，添加序号问题，示例：
+dm_field, dmxh_field
+xxxx，1
+xxxx，2
+yyyy，1
+yyyy，2
+yyyy，3
+"""
 
 import arcpy
 # 定义一个函数，参数为feature_class, zddm_field, zddmxh_field
-def number_zddm(feature_class, zddm_field, zddmxh_field):
-    # 创建一个字典，用来存储ZDDM和ZDDMXH的对应关系
-    zddm_dict = {}
+def number_zddm(feature_class, dm_field, dmxh_field):
+    # 创建一个字典，用来存储DM和DMXH（序号）的对应关系
+    dm_dict = {}
     # 创建一个游标，用来遍历要素类中的每一行
-    with arcpy.da.UpdateCursor(feature_class, [zddm_field, zddmxh_field]) as cursor:
+    with arcpy.da.UpdateCursor(feature_class, [dm_field, dmxh_field]) as cursor:
         # 对于每一行
         for row in cursor:
-            # 获取当前行的ZDDM值
-            zddm = row[0]
+            # 获取当前行的DM值
+            dm = row[0]
             # 如果这个值已经在字典中，说明已经有一个序号了
-            if zddm in zddm_dict:
+            if dm in dm_dict:
                 # 就把这个序号加一，作为新的序号
-                zddmxh = zddm_dict[zddm] + 1
-                # 把这个序号赋值给当前行的ZDDMXH字段
-                row[1] = zddmxh
-                # 并且更新字典中的这个ZDDM值对应的序号
-                zddm_dict[zddm] = zddmxh
+                dmxh = dm_dict[dm] + 1
+                # 把这个序号赋值给当前行的DMXH字段
+                row[1] = dmxh
+                # 并且更新字典中的这个DM值对应的序号
+                dm_dict[dm] = dmxh
             # 否则，说明这是一个新的ZDDM值，还没有序号
             else:
                 # 就把序号设为1
-                zddmxh = 1
+                dmxh = 1
                 # 把这个序号赋值给当前行的ZDDMXH字段
-                row[1] = zddmxh
+                row[1] = dmxh
                 # 并且把这个ZDDM和ZDDMXH的对应关系存入字典中
-                zddm_dict[zddm] = zddmxh
+                dm_dict[dm] = dmxh
             # 更新当前行的值
             cursor.updateRow(row)
 
 # 调用函数，传入参数，例如：
-number_zddm(r"C:\Users\18292\Desktop\zrz\610632ZRZ.shp", "ZDDM", "ZDDMXH")
+feature_class = arcpy.GetParameterAsText(0)
+dm_field = arcpy.GetParameterAsText(1)
+dmxh_field = arcpy.GetParameterAsText(2)
+number_zddm(feature_class, dm_field, dmxh_field)
